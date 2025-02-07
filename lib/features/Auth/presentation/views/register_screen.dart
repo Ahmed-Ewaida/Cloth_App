261cc54ebf59fb8a9fb9b0d30c6cb6b5f940f47e
Mohamed-Ismail-Ahmed <mohamed14101998i@gmail.com>
2025-02-08 01:41:48 +0200

    Mohamed

--- lib/features/Auth/presentation/views/register_screen.dart
+++ lib/features/Auth/presentation/views/register_screen.dart
@@ -1,3 +1,13 @@
-llkkjkjnkj
-Ahmed
-    hjjhbjkkjnkkj
+import 'package:cloyhapp/features/Auth/presentation/views/widgets/register_body.dart';
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      body: RegisterBody(),
+    ));
+  }
+}
