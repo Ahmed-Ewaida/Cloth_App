6d8929b2cc3761ea9c08d9669722c5caf61d828f
SamaaIbrahim56 <ibrahimsama682@gmail.com>
2025-02-09 00:09:36 +0200

    sama catalog

--- lib/core/Assets/assets_images.dart
+++ lib/core/Assets/assets_images.dart
@@ -1,9 +1,9 @@
 abstract class AppAssets {
-  static const baseImage = "images";
+  static const baseImage = "assets/images";
   static const splash = "$baseImage/splash.png";
   static const google = "$baseImage/google.png";
   static const facebook = "$baseImage/facebook.png";
   static const category = "$baseImage/img.png";
   static const testImage = "$baseImage/testImage.png";
-  static const pullover = "$baseImage/pullover with model.png";
+  static const pullover = "$baseImage/pullover.png";
 }
