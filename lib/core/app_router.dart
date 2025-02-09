7b746a56520fb6ceda056432601abcdb121fb41c
Ahmed Ewaida <ahmedewaida234@gmail.com>
2025-02-10 00:21:27 +0200

    ahmed comment

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -2,6 +2,7 @@ import 'package:cloyhapp/features/Auth/presentation/views/forget_password.dart';
 import 'package:cloyhapp/features/Auth/presentation/views/login_screen.dart';
 import 'package:cloyhapp/features/Auth/presentation/views/register_screen.dart';
 import 'package:cloyhapp/features/Category/presentation/views/category_screen.dart';
+import 'package:cloyhapp/features/Home/presentation/main_home_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../features/Category/presentation/views/widgets/search_categort_body.dart';
@@ -15,7 +16,7 @@ class AppRouter{
       case 'registerscreen':return MaterialPageRoute(builder: (_)=>RegisterScreen());
       case 'loginscreen':return MaterialPageRoute(builder: (_)=>LoginScreen());
       case 'forgetscreen':return MaterialPageRoute(builder: (_)=>ForgetPassword());
-      case 'homescreen':return MaterialPageRoute(builder: (_)=>HomeScreen());
+      case 'homescreen':return MaterialPageRoute(builder: (_)=>MainHomeScreen());
       case 'categgoryscreen':return MaterialPageRoute(builder: (_)=>CategoryScreen());
       case 'SearchCategortBody':return MaterialPageRoute(builder: (_)=>SearchCategortBody());
       // case 'detailsScreen':return MaterialPageRoute(builder: (_)=>DetailsScreen());
