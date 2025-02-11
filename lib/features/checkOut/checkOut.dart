c5273c7d0c9f26badd67f833d419d4b2780343ab
amira1972 <amiraahmed24301@gmail.com>
2025-02-11 22:32:47 +0200

    comment amira

---
+++ lib/features/checkOut/checkOut.dart
@@ -0,0 +1,11 @@
+import 'package:cloyhapp/features/checkOut/presentation/view/checkOut_body.dart';
+import 'package:flutter/material.dart';
+
+class CheckOut extends StatelessWidget {
+  const CheckOut({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CheckOutBody();
+  }
+}
