c14c8ff5ed618fcbb445c0d3448e1f209e50b529
Ahmed Ewaida <ahmedewaida234@gmail.com>
2025-02-11 19:23:58 +0200

    ahmed comment

---
+++ lib/features/Prodect/presentation/views/prodect_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ProdectScreen extends StatelessWidget {
+  const ProdectScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
