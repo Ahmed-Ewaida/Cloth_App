cad153143e24611fc810d5ebb3dd84e4fe6e3b41
HP <mohamed14101998i@gmail.com>
2025-02-14 03:40:38 +0200

    Mohamed

--- lib/features/settings/presentation/views/widgets/settings_Change_Paasword.dart
+++ lib/features/settings/presentation/views/widgets/settings_Change_Paasword.dart
@@ -1,39 +1,41 @@
 import 'package:cloyhapp/features/settings/presentation/views/widgets/Settings_Bottomsheet.dart';
 import 'package:flutter/material.dart';
+
 class SettingsChangePaasword extends StatefulWidget {
   const SettingsChangePaasword({super.key});
+
   @override
   State<SettingsChangePaasword> createState() => _SettingsChangePaaswordState();
 }
+
 class _SettingsChangePaaswordState extends State<SettingsChangePaasword> {
-  final scaffoldKey=GlobalKey<ScaffoldState>();
   bool bottomsheet = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      body: Row(
+    return
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Text("Password",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16),),
-          FloatingActionButton(
-            backgroundColor: Colors.white,
-            onPressed: (){
-              if(bottomsheet){
-                Navigator.pop(context);
-                setState(() {
-                  bottomsheet = false;
-                });
-              }else{
-                scaffoldKey.currentState?.showBottomSheet( (context){
-                  return SettingsBottomsheet();
-                });
-                bottomsheet =! bottomsheet;
-              }}, child: Text("Change",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,color: Color(0xff9B9B9B)),),
-          ),
-        ],
-      )
+        Text("Password",
+        style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),),
+    TextButton(
+    onPressed: (){
+    if(bottomsheet){
+    Navigator.pop(context);
+    setState(() {
+    bottomsheet = false;
+    });
+    }else{
+    Scaffold.of(context)
+        .showBottomSheet((BuildContext context) => SettingsBottomsheet());
+    }
+    bottomsheet =! bottomsheet;
+     },child:Text("Change",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,color: Color(0xff9B9B9B)),),),
 
-    );
-  }
-}
+  ],
+
+  );
+
+
+}}
