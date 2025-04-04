35230e72f4b3b32ce08b63dce4e771c448ae9c7c
AHMED-EWAIDA\Ahmed Ewaida <ahmedewaida234@gmail.com>
2025-04-05 00:38:30 +0200

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,3 @@
-import 'package:cloyhapp/features/Catalog/presentation/views/catalog_screen.dart';
-import 'package:cloyhapp/features/Favorite/presentation/views/Fav_screen.dart';
-import 'package:cloyhapp/features/Home/presentation/main_home_screen.dart';
-import 'package:cloyhapp/features/MyOrders/presentation/views/MyOrder_screen.dart';
-import 'package:cloyhapp/features/MyOrders/presentation/views/widgets/MyOrder_body.dart';
-import 'package:cloyhapp/features/MyProfile/presentation/views/MyProfile_Screen.dart';
-import 'package:cloyhapp/features/Prodect/presentation/views/prodect_card_screen.dart';
-import 'package:cloyhapp/features/Prodect/presentation/views/prodect_rating_screen.dart';
-import 'package:cloyhapp/features/Prodect/presentation/views/prodect_screen.dart';
-import 'package:cloyhapp/features/checkOut/presentation/view/checkOut.dart';
-import 'package:cloyhapp/features/payment/presentation/views/payment_cards.dart';
-import 'package:cloyhapp/features/settings/presentation/views/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cloyhapp/core/app_router.dart';
 
@@ -17,6 +5,7 @@ void main() {
   runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
   MyApp({super.key});
   AppRouter appRouter = AppRouter();
@@ -26,13 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // title: 'Flutter Demo',
-      theme: ThemeData.light(
-      ),
-       debugShowCheckedModeBanner: false,
-       onGenerateRoute: appRouter.generateRoute,
-
-
-
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
